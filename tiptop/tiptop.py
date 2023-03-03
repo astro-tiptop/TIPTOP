8d5dd7fcaa81abcd8eb695a128092a27c6c9085e
Simon Conseil <simon.conseil@lam.fr>
2023-03-03 15:59:58 +0100

    Update p3 import

--- tiptop/tiptop.py
+++ tiptop/tiptop.py
@@ -1,8 +1,8 @@
 import os
 import numpy as np
 from matplotlib import rc
-from P3.aoSystem.fourierModel import *
-from P3.aoSystem.FourierUtils import *
+from p3.aoSystem.fourierModel import *
+from p3.aoSystem.FourierUtils import *
 from configparser import ConfigParser
 import yaml
 
@@ -12,11 +12,11 @@ from datetime import datetime
 
 rc("text", usetex=False)
 
-def overallSimulation(path, parametersFile, outputDir, outputFile, doConvolve=False, 
+def overallSimulation(path, parametersFile, outputDir, outputFile, doConvolve=False,
                       doPlot=False, verbose=False, returnRes=False, addSrAndFwhm=False):
     """
     function to run the entire tiptop simulation based on the imput file
-    
+
     :param path2param: required, path to the parameter file
     :type path2param: str
     :param paramFileName: required, name of the parameter file to be used without the extention
@@ -29,22 +29,22 @@ def overallSimulation(path, parametersFile, outputDir, outputFile, doConvolve=Fa
     :type doPlot: bool
     :param verbose: optional default: False, If you want all messages set this to True
     :type verbose: bool
-    :param returnRes: optionnal default: False, The function will return the result in the environment if set to True, else it saves the result only in a .fits file. 
+    :param returnRes: optionnal default: False, The function will return the result in the environment if set to True, else it saves the result only in a .fits file.
     :type returnRes: bool
-    :param addSrAndFwhm: optionnal default: False, The function will add in the header of the fits file SR anf FWHM for each PSF. 
+    :param addSrAndFwhm: optionnal default: False, The function will add in the header of the fits file SR anf FWHM for each PSF.
     :type addSrAndFwhm: bool
 
     :return: TBD
     :rtype: TBD
 
     """
-    
+
     # initiate the parser
     fullPathFilename_ini = os.path.join(path, parametersFile + '.ini')
     fullPathFilename_yml = os.path.join(path, parametersFile + '.yml')
 
     # initialize jitter_FWHM variable with a default value
-    jitter_FWHM = None 
+    jitter_FWHM = None
 
     if os.path.exists(fullPathFilename_yml):
         with open(fullPathFilename_yml) as f:
@@ -58,7 +58,7 @@ def overallSimulation(path, parametersFile, outputDir, outputFile, doConvolve=Fa
             wvl = wvl_temp     # lambda
         zenithSrc  = my_yaml_dict['sources_science']['Zenith']
         azimuthSrc = my_yaml_dict['sources_science']['Azimuth']
-        
+
         # it checks if LO parameters are set and then it acts accordingly
         if 'sensor_LO' in my_yaml_dict.keys():
             LOisOn = True
@@ -68,7 +68,7 @@ def overallSimulation(path, parametersFile, outputDir, outputFile, doConvolve=Fa
             nNaturalGS = 0
             if verbose: print('LO part is not present')
 
-        if LOisOn: 
+        if LOisOn:
             LO_wvl_temp = my_yaml_dict['sources_LO']['Wavelength']
             if isinstance(LO_wvl_temp, list):
                 LO_wvl = LO_wvl_temp[0]  # lambda
@@ -78,13 +78,13 @@ def overallSimulation(path, parametersFile, outputDir, outputFile, doConvolve=Fa
             LO_az      = my_yaml_dict['sources_LO']['Azimuth']
             LO_fluxes  = my_yaml_dict['sensor_LO']['NumberPhotons']
             fr         = my_yaml_dict['RTC']['SensorFrameRate_LO']
-            
+
         if 'jitter_FWHM' in my_yaml_dict['telescope'].keys():
             jitter_FWHM = my_yaml_dict['telescope']['jitter_FWHM']
 
         fao = fourierModel( fullPathFilename_yml, calcPSF=False, verbose=False
                            , display=False, getPSDatNGSpositions=True)
-        
+
     elif os.path.exists(fullPathFilename_ini):
         parser           = ConfigParser()
         parser.read(fullPathFilename_ini);
@@ -112,24 +112,24 @@ def overallSimulation(path, parametersFile, outputDir, outputFile, doConvolve=Fa
                 LO_wvl = LO_wvl_temp[0]  # lambda
             else:
                 LO_wvl = LO_wvl_temp     # lambda
-            LO_zen     = eval(parser.get('sources_LO', 'Zenith')) 
+            LO_zen     = eval(parser.get('sources_LO', 'Zenith'))
             LO_az      = eval(parser.get('sources_LO', 'Azimuth'))
             LO_fluxes  = eval(parser.get('sensor_LO', 'NumberPhotons'))
             fr         = eval(parser.get('RTC', 'SensorFrameRate_LO'))
         if parser.has_option('telescope', 'jitter_FWHM'):
             jitter_FWHM = eval(parser.get('telescope', 'jitter_FWHM'))
-                
+
         fao = fourierModel( fullPathFilename_ini, calcPSF=False, verbose=False
                            , display=False, getPSDatNGSpositions=True, computeFocalAnisoCov=False)
     else:
         raise FileNotFoundError('No .yml or .ini can be found in '+ path)
 
-    if LOisOn:        
+    if LOisOn:
         # NGSs positions
         NGS_flux = []
         polarNGSCoordsList = []
         for aFlux, aZen, aAz in zip(LO_fluxes, LO_zen, LO_az):
-            polarNGSCoordsList.append([aZen, aAz])   
+            polarNGSCoordsList.append([aZen, aAz])
             NGS_flux.append(LO_fluxes[0]*fr)
             polarNGSCoords     = np.asarray(polarNGSCoordsList)
             nNaturalGS         = polarNGSCoords.shape[0]
@@ -137,7 +137,7 @@ def overallSimulation(path, parametersFile, outputDir, outputFile, doConvolve=Fa
     pp                 = polarToCartesian(np.array( [zenithSrc, azimuthSrc]))
     xxPointigs         = pp[0,:]
     yyPointigs         = pp[1,:]
-        
+
     # High-order PSD caculations at the science directions and NGSs directions
     PSD                = fao.PSD # in nm^2
     nPointings         = pp.shape[1]
@@ -146,12 +146,12 @@ def overallSimulation(path, parametersFile, outputDir, outputFile, doConvolve=Fa
 
     PSD           = PSD.transpose()
     N             = PSD[0].shape[0]
-    freq_range    = fao.ao.cam.fovInPix*fao.freq.PSDstep 
+    freq_range    = fao.ao.cam.fovInPix*fao.freq.PSDstep
     pitch         = 1/freq_range
     grid_diameter = pitch*N
     sx            = int(2*np.round(tel_radius/pitch))
     dk            = 1e9*fao.freq.kcMax_/fao.freq.resAO
-    
+
     # Define the pupil shape
     mask = Field(wvl, N, grid_diameter)
     mask.sampling = cp.asarray(congrid(fao.ao.tel.pupil, [sx, sx]))
@@ -172,7 +172,7 @@ def overallSimulation(path, parametersFile, outputDir, outputFile, doConvolve=Fa
             psd.sampling   = cp.asarray( computedPSD / dk**2) # the PSD must be provided in m^2.m^2
             psdArray.append(psd)
             # Get the PSF
-            psfLE          = longExposurePsf(mask, psd )     
+            psfLE          = longExposurePsf(mask, psd )
             psfLongExpArr.append(psfLE)
             # Get SR and FWHM in mas at the NGSs positions at the sensing wavelength
             SR             = np.exp(-computedPSD.sum()* scaleFactor) # Strehl-ratio at the sensing wavelength
@@ -184,7 +184,7 @@ def overallSimulation(path, parametersFile, outputDir, outputFile, doConvolve=Fa
             if verbose:
                 print('SR(@',int(wavelength*1e9),'nm)  :', SR)
                 print('FWHM(@',int(wavelength*1e9),'nm):', FWHM)
-            
+
         return NGS_SR, psdArray, psfLongExpArr, NGS_FWHM_mas
 
     # HO PSF
@@ -195,7 +195,7 @@ def overallSimulation(path, parametersFile, outputDir, outputFile, doConvolve=Fa
                                                                                                  fao.freq.psInMas[0],
                                                                                                  scaleFactor=(2*np.pi*1e-9/wvl)**2,
                                                                                                  verbose=verbose)
-    
+
     if LOisOn == False or (doConvolve == False and returnRes == False):
         results = []
         for psfLongExp in psfLongExpPointingsArr:
@@ -205,20 +205,20 @@ def overallSimulation(path, parametersFile, outputDir, outputFile, doConvolve=Fa
                                residualToSpectrum(ellp, wvl, N, 1/(fao.ao.cam.fovInPix * fao.freq.psInMas[0]))))
             else:
                 results.append(psfLongExp)
-            
+
     else:
         # LOW ORDER PART
         if verbose:
             print('******** LO PART')
         psInMas_NGS        = fao.freq.psInMas[0] * (LO_wvl/wvl) #airy pattern PSF FWHM
-        
+
         if verbose:
             print('******** HO PSF - NGS directions')
         NGS_SR, psdArray, psfLE_NGS, NGS_FWHM_mas = psdSetToPsfSet(PSD[-nNaturalGS:],
                                                                    LO_wvl, psInMas_NGS,
                                                                    scaleFactor=(2*np.pi*1e-9/LO_wvl)**2,
                                                                    verbose=verbose)
-        
+
         cartPointingCoords = np.dstack( (xxPointigs, yyPointigs) ).reshape(-1, 2)
         cartNGSCoordsList = []
         for i in range(nNaturalGS):
@@ -226,13 +226,13 @@ def overallSimulation(path, parametersFile, outputDir, outputFile, doConvolve=Fa
         cartNGSCoords = np.asarray(cartNGSCoordsList)
         mLO           = MavisLO(path, parametersFile,verbose=verbose)
         Ctot          = mLO.computeTotalResidualMatrix(np.array(cartPointingCoords)
-                                                       , cartNGSCoords, NGS_flux, 
+                                                       , cartNGSCoords, NGS_flux,
                                                        NGS_SR, NGS_FWHM_mas)
-        
+
         if returnRes == False:
             cov_ellipses       = mLO.ellipsesFromCovMats(Ctot)
             if verbose:
-                for n in range(cov_ellipses.shape[0]): 
+                for n in range(cov_ellipses.shape[0]):
                     print('cov_ellipses #',n,': ',cov_ellipses[n,:], ' (unit: rad, mas, mas)')
             # FINAl CONVOLUTION
             if verbose:
@@ -240,9 +240,9 @@ def overallSimulation(path, parametersFile, outputDir, outputFile, doConvolve=Fa
             results = []
             for ellp, psfLongExp in zip(cov_ellipses, psfLongExpPointingsArr):
                 results.append(convolve(psfLongExp, residualToSpectrum(ellp, wvl
-                                                                       , N, 1/(fao.ao.cam.fovInPix 
+                                                                       , N, 1/(fao.ao.cam.fovInPix
                                                                                * fao.freq.psInMas[0]))))
-    
+
     if doPlot:
         if LOisOn and doConvolve and returnRes == False:
             tiledDisplay(results)
@@ -250,18 +250,18 @@ def overallSimulation(path, parametersFile, outputDir, outputFile, doConvolve=Fa
         else:
             results[0].standardPlot(True)
 
-    # save PSF cube in fits    
+    # save PSF cube in fits
     hdul1 = fits.HDUList()
     cube =[]
     hdul1.append(fits.PrimaryHDU())
     for img in results:
         cube.append(img.sampling)
-    
+
     if returnRes:
         HO_res = np.sqrt(np.sum(PSD[:-nNaturalGS],axis=(1,2)))
         if LOisOn:
             LO_res = np.sqrt(np.trace(Ctot,axis1=1,axis2=2))
-    
+
             return HO_res, LO_res
         else:
             return HO_res
@@ -273,23 +273,23 @@ def overallSimulation(path, parametersFile, outputDir, outputFile, doConvolve=Fa
         temp = fao.ao.atm.spectrum(k) * pf
         psdOL.sampling = cp.asarray(fao.ao.atm.spectrum(k) * pf * (fao.freq.wvlRef/np.pi)**2) # the PSD must be provided in m^2.m^2
         # Get the OPEN-LOOP PSF
-        psfOL = longExposurePsf(mask, psdOL) 
+        psfOL = longExposurePsf(mask, psdOL)
         if doPlot:
             fig, ax1 = plt.subplots(1,1)
             im = ax1.imshow(np.log(np.abs(psfOL.sampling) + 1e-20), cmap='hot')
             ax1.set_title('open loop PSF', color='black')
-            
+
         # save PSF cube in fits
         hdul1 = fits.HDUList()
         cube =[]
         hdul1.append(fits.PrimaryHDU())
         for img in results:
             cube.append(img.sampling)
-        
+
         cube = np.array(cube)
         hdul1.append(fits.ImageHDU(data=cube))
         hdul1.append(fits.ImageHDU(data=psfOL.sampling)) # append open-loop PSF
-        
+
         #############################
         # header
         hdr0 = hdul1[0].header
@@ -323,7 +323,7 @@ def overallSimulation(path, parametersFile, outputDir, outputFile, doConvolve=Fa
                 hdr1['SR'+str(i).zfill(4)]   = getStrehl(cube[i,:,:], fao.ao.tel.pupil, fao.freq.sampRef)
             for i in range(cube.shape[0]):
                 hdr1['FWHM'+str(i).zfill(4)] = getFWHM(cube[i,:,:], fao.freq.psInMas[0], method='contour', nargout=1)
-            
+
         # header of the coordinates
         hdr2 = hdul1[2].header
         hdr2['TIME'] = now.strftime("%Y%m%d_%H%M%S")
