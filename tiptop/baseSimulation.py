8e0c7236ab66b4a69110722b2f9eb4b5dbbedf61
Guido Agapito <guido.agapito@inaf.it>
2025-03-04 14:03:39 +0100

    (GA) bug fix in SR computation (#170)
    * (GA) bug fix in SR computation
    
    * (GA) img.sampling in ee computation

--- tiptop/baseSimulation.py
+++ tiptop/baseSimulation.py
@@ -821,10 +821,10 @@ class baseSimulation(object):
                 fwhm = []
                 ee = []
                 for img in results:
-                    sr.append(getStrehl(img.sampling, self.fao.ao.tel.pupil, 2*self.tel_radius, samp, method='max', psfInOnePix=True))
+                    sr.append(getStrehl(img.sampling, self.fao.ao.tel.pupil, samp, method='max', psfInOnePix=True))
                     fwhm.append(getFWHM(img.sampling, self.psInMas, method='contour', nargout=1))
                     if self.ensquaredEnergy:
-                        ee_ = cpuArray(getEnsquaredEnergy(self.cubeResultsArray[i,:,:]))
+                        ee_ = cpuArray(getEnsquaredEnergy(img.sampling))
                         rr_ = np.arange(1, ee_.shape[0]*2, 2) * self.psInMas * 0.5
                     else:
                         ee_,rr_ = getEncircledEnergy(img.sampling, pixelscale=self.psInMas,
